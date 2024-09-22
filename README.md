3585abc37049c98522c7a895ed0b5d7882a0a143
이관우 (Brian.Lee) <109894644+dlrhksdn@users.noreply.github.com>
2024-09-22 05:30:51 +0000

    base

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# supabase
+supabase에서 기억하면 좋을것들
+
+1. supabase에서 authentication에서 url redirect가 있어야 신뢰한다고 믿고
+로그인을 시켜줌
+
+2. table에 column을 추가할때 user_id를 만들경우
+*1. type : uuid / value : auth.uid()
+*2. 링크를 눌러서 schema를 auth로 바꾸고 reference를 users로 바꾸고
+user_id -> id로 바꾸고 맨밑에 삭제되었을때를 cascade로 바꾸면\
+ 계정이 사라지면 그 계정이 작성한 모든것이 같이 사라지도록 함
+
+3. authentication에서 policy로 들어가면 정책을 설정할 수 있음
+*. 우선 내가 원하는 정책을 미리 생각해둬야함
+(모두가 읽을 수 있게 하는가? : 0)
+(글을 지울 수 있는가? : 소유자만)
+...등등
---
+++ supabase2/server.js
@@ -0,0 +1,64 @@
+/**
+ * node app.js로 실행하거나
+ * npm install -g nodemon을 설치해서 (이때 -g는 global)
+ * nodemon app.js로 실행             (nodemon은 변경사항을 실행중에도 반영되도록 함)
+ * ctrl + c로 종료
+ */
+//supabase 연결하는법, 글쓰기 반영
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config(); // dotenv 패키지 로드
+
+const { createClient } = require('@supabase/supabase-js');
+const supabaseUrl = 'https://qvceuxiwdjbyuqzywptl.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, 'src', req.url === '/' ? 'pages/index.html' : req.url);
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (extname) { //css나 다른 형식들 반영
+    case '.css':
+      contentType = 'text/css';
+      filePath = path.join(__dirname, 'src', 'style', 'index.css');
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => { //오류시 보여주는 페이지
+    if (err) {
+      if (err.code == 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found</h1>', 'utf8');
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf8');
+    }
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
